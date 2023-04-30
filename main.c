17ac5e8d098916da08e266299b418649e71e5d74
kvasa <kvasan2104@gmail.com>
2023-04-30 15:24:49 +0300

    json parser implemented (tested)

--- main.c
+++ main.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
+#include "json_parser.h"
+#include "database.h"
 
 int main() {
-    printf("Hello, World!\n");
-    return 0;
+    Student_t *database;
+    database = (Student_t *) malloc(DB_CAPACITY * (sizeof(Student_t)));
+    int number_of_students = 0;
+    json_parser("../database.json", database, &number_of_students);
 }
